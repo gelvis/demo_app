85980321dca78f8b2761862205b6fb503d45395a
Gunnar Solheim <gs@svorka.no>
2012-11-23 12:39:08 +0100

    sadf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '5978e0bb1fcd9e5458f8120fed4380e011b1f9297a5a6d56b167bb2c5111dcadb9d8033d144d64611af24357f0bad4cfb7355ec12f3d29bb6189fae7dbbc94b6'
